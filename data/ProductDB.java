2b7d0199ab254c6b030ceb0e7e27de11817c4d3b
Majuska93 <33498002+Majuska93@users.noreply.github.com>
2017-11-09 14:52:05 +0100

    Update ProductDB.java

--- data/ProductDB.java
+++ data/ProductDB.java
@@ -1,5 +1,28 @@
 package data;
 
-public class ProductDB {
+import java.util.ArrayList;
+import view.*;
+import control.*;
+import model.*;
 
+public class ProductDB {
+private ArrayList<Product> productList = new ArrayList<Product>();
+//constructor
+public ProductDB() {
+}
+
+public Product browseByProductID(String productID) {
+	for(int i=0;i<productList.size();i++) {
+		if(productID.equals(productList.get(i).getProductID())) {
+			return productList.get(i);
+		}
+	}
+	return new Product();
+}
+public void assignProductList() {
+	while(/*sth is there*/) {
+		Product product = new Product(/*bierz szczegóły productu*/);
+		productList.add(product);
+	}
+}
 }
